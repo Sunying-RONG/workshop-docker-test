2ffc28e07b8d0ff71a408f097b6201f16204605f
sunying <sunying.rong@hotmail.com>
2019-07-13 13:52:43 +0800

    test2

--- run_test.py
+++ run_test.py
@@ -10,5 +10,8 @@ class TestRun(unittest.TestCase):
     def test_executeOne(self):
       self.assertEqual(2, run.execute(-1))
 
+    def test_executeTwo(self):
+      self.assertEqual(2, run.execute(2))
+
 if __name__ == '__main__':
     unittest.main()
